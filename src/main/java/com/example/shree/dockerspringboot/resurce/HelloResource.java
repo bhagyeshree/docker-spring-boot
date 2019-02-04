19f914f84069aa1404072ded81bdb9ab36aa9272
shree <bhagyeshree.gaikwad@gmail.com>
2019-02-04 13:15:35 -0500

    first commit

---
+++ src/main/java/com/example/shree/dockerspringboot/resurce/HelloResource.java
@@ -0,0 +1,19 @@
+package com.example.shree.dockerspringboot.resurce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResource {
+
+
+    @GetMapping
+    public String hello()
+    {
+        return "Hello Youtube";
+
+    }
+
+}
